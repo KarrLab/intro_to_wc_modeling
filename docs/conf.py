8a01487e734625422962ae4a01935b61fcc36f01
Jonathan Karr <jonrkarr@gmail.com>
2018-03-17 11:45:05 -0400

    updating copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import datetime
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -71,7 +72,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'An introduction to whole-cell modeling'
-copyright = '2017, Karr Lab'
+copyright = u'{}, Karr Lab'.format(datetime.datetime.now().year)
 author = 'Jonathan Karr and Arthur Goldberg'
 
 # The version info for the project you're documenting, acts as replacement for
